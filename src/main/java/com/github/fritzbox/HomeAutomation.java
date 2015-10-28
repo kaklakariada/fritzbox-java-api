149708dc30d1a4eaeda00a89f0a15de4b0a316f5
kaklakariada <kaklakariada@users.noreply.github.com>
2015-10-28 11:18:19 +0100

    added switch actor methods

--- src/main/java/com/github/fritzbox/HomeAutomation.java
+++ src/main/java/com/github/fritzbox/HomeAutomation.java
@@ -1,6 +1,11 @@
 package com.github.fritzbox;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,9 +24,75 @@ public class HomeAutomation {
     }
 
     public DeviceList getDeviceListInfos() {
-        final DeviceList deviceList = fritzbox.getForObject(HOME_AUTOMATION_PATH,
-                Collections.singletonMap("switchcmd", "getdevicelistinfos"), DeviceList.class);
-        LOG.trace("Found {} devices, devicelist version: {}", deviceList.getDevices().size(), deviceList.getVersion());
+        final String command = "getdevicelistinfos";
+        final DeviceList deviceList = executeCommand(command, DeviceList.class);
+        LOG.trace("Found {} devices, devicelist version: {}", deviceList.getDevices().size(), deviceList.getApiVersion());
         return deviceList;
+    }
+
+    private <T> T executeCommand(String command, Class<T> responseType) {
+        final Map<String, String> args = Collections.singletonMap("switchcmd", command);
+        return fritzbox.getForObject(HOME_AUTOMATION_PATH, args, responseType);
+    }
+
+    private String executeDeviceCommand(String deviceAin, String command, String parameter) {
+        final Map<String, String> args = new HashMap<>();
+        args.put("ain", deviceAin);
+        args.put("switchcmd", command);
+        if (parameter != null) {
+            args.put("param", parameter);
+        }
+        final String result = fritzbox.getForObject(HOME_AUTOMATION_PATH, args, String.class).trim();
+        LOG.debug("Executed command '{}' with parameter '{}' for device '{}', got result '{}'", command, parameter,
+                deviceAin, result);
+        return result;
+    }
+
+    public List<String> getSwitchList() {
+        final String switches = executeCommand("getswitchlist", String.class);
+        final List<String> idList = Arrays.stream(switches.split(",")) //
+                .map(String::trim) //
+                .collect(Collectors.toList());
+        LOG.trace("Got switch list string '{}': {}", switches, idList);
+        return idList;
+    }
+
+    public void switchPowerState(String deviceAin, boolean on) {
+        final String command = on ? "setswitchon" : "setswitchoff";
+        executeDeviceCommand(deviceAin, command, null);
+    }
+
+    public void togglePowerState(String deviceAin) {
+        executeDeviceCommand(deviceAin, "setswitchtoggle", null);
+    }
+
+    public String getSwitchState(String deviceAin) {
+        return executeDeviceCommand(deviceAin, "getswitchstate", null);
+    }
+
+    public String getSwitchPresent(String deviceAin) {
+        return executeDeviceCommand(deviceAin, "getswitchpresent", null);
+    }
+
+    public String getSwitchName(String deviceAin) {
+        return executeDeviceCommand(deviceAin, "getswitchname", null);
+    }
+
+    public float getTemperature(String deviceAin) {
+        final String string = executeDeviceCommand(deviceAin, "gettemperature", null);
+        final int centiDegree = Integer.parseInt(string);
+        return centiDegree / 10F;
+    }
+
+    public float getSwitchPowerWatt(String deviceAin) {
+        final String string = executeDeviceCommand(deviceAin, "getswitchpower", null);
+        final int powerMilliWatt = Integer.parseInt(string);
+        return powerMilliWatt / 1000F;
+    }
+
+    public int getSwitchEnergyWattHour(String deviceAin) {
+        final String string = executeDeviceCommand(deviceAin, "getswitchenergy", null);
+        final int energyWattHours = Integer.parseInt(string);
+        return energyWattHours;
     }
 }
--- src/main/java/com/github/fritzbox/TestDriver.java
+++ src/main/java/com/github/fritzbox/TestDriver.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Properties;
 
 import org.slf4j.Logger;
@@ -14,9 +15,7 @@ import org.springframework.web.client.RestTemplate;
 import com.github.fritzbox.http.CustomHostnameVerifierHttpRequestFactory;
 import com.github.fritzbox.http.NullHostnameVerifier;
 import com.github.fritzbox.http.TrustSelfSignedCertificates;
-import com.github.fritzbox.model.homeautomation.Device;
 import com.github.fritzbox.model.homeautomation.DeviceList;
-import com.github.fritzbox.model.homeautomation.PowerMeter;
 
 public class TestDriver {
     private final static Logger LOG = LoggerFactory.getLogger(TestDriver.class);
@@ -31,22 +30,37 @@ public class TestDriver {
                 new RestTemplate(new CustomHostnameVerifierHttpRequestFactory(new NullHostnameVerifier())));
         session.login(username, password);
         final HomeAutomation homeAutomation = new HomeAutomation(session);
+
         final DeviceList devices = homeAutomation.getDeviceListInfos();
         LOG.info("Found {} devices", devices.getDevices().size());
         devices.getDevices().stream().forEach(d -> LOG.info("\t{}", d));
+
+        final List<String> ids = homeAutomation.getSwitchList();
+        LOG.info("Found {} device ids: {}", ids.size(), ids);
 
         if (devices.getDevices().isEmpty()) {
             session.logout();
             return;
         }
-        while (true) {
-            final Device device = homeAutomation.getDeviceListInfos().getDevices().get(0);
-            final PowerMeter powerMeter = device.getPowerMeter();
-            LOG.debug("State: {}, temp: {}°C, power: {}W, energy: {}Wh",
-                    device.getSwitchState().isState() ? "on" : "off", device.getTemperature().getCelsius(),
-                    powerMeter.getPowerWatt(), powerMeter.getEnergyWattHours());
-            Thread.sleep(1000);
-        }
+
+        final String ain = ids.get(0);
+        // homeAutomation.switchPowerState(ain, false);
+        // homeAutomation.togglePowerState(ain);
+        LOG.info("Switch {} has present state '{}'", ain, homeAutomation.getSwitchPresent(ain));
+        LOG.info("Switch {} has state '{}'", ain, homeAutomation.getSwitchState(ain));
+        LOG.info("Switch {} uses {}W", ain, homeAutomation.getSwitchPowerWatt(ain));
+        LOG.info("Switch {} has used {}Wh", ain, homeAutomation.getSwitchEnergyWattHour(ain));
+        LOG.info("Switch {} has name '{}'", ain, homeAutomation.getSwitchName(ain));
+        LOG.info("Switch {} has temperature {}°C", ain, homeAutomation.getTemperature(ain));
+
+        // while (true) {
+        // final Device device = homeAutomation.getDeviceListInfos().getDevices().get(0);
+        // final PowerMeter powerMeter = device.getPowerMeter();
+        // LOG.debug("State: {}, temp: {}°C, power: {}W, energy: {}Wh",
+        // device.getSwitchState().isState() ? "on" : "off", device.getTemperature().getCelsius(),
+        // powerMeter.getPowerWatt(), powerMeter.getEnergyWattHours());
+        // Thread.sleep(1000);
+        // }
     }
 
     private static Properties readConfig(Path file) {
