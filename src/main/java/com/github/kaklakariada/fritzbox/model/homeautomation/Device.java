e07c3486f8ec4b3b46e8ed17e6aee885836e7f6e
Wolfgang Fahl <wf@bitplan.com>
2018-01-23 11:55:59 +0100

    fixes issues with repeater by setting required to false for some elements in device

--- src/main/java/com/github/kaklakariada/fritzbox/model/homeautomation/Device.java
+++ src/main/java/com/github/kaklakariada/fritzbox/model/homeautomation/Device.java
@@ -24,81 +24,83 @@ import org.simpleframework.xml.Root;
 @Root(name = "device")
 public class Device {
 
-    @Attribute(name = "identifier", required = true)
-    private String identifier;
-    @Attribute(name = "id")
-    private String id;
-    @Attribute(name = "functionbitmask")
-    private int functionBitmask;
-    @Attribute(name = "fwversion")
-    private String firmwareVersion;
-    @Attribute(name = "manufacturer")
-    private String manufacturer;
+  @Attribute(name = "identifier", required = true)
+  private String identifier;
+  @Attribute(name = "id")
+  private String id;
+  @Attribute(name = "functionbitmask")
+  private int functionBitmask;
+  @Attribute(name = "fwversion")
+  private String firmwareVersion;
+  @Attribute(name = "manufacturer")
+  private String manufacturer;
 
-    @Attribute(name = "productname")
-    private String productName;
+  @Attribute(name = "productname")
+  private String productName;
 
-    @Element(name = "present")
-    private String present;
-    @Element(name = "name")
-    private String name;
+  @Element(name = "present")
+  private String present;
+  @Element(name = "name")
+  private String name;
 
-    @Element(name = "switch")
-    private SwitchState switchState;
-    @Element(name = "powermeter")
-    private PowerMeter powerMeter;
-    @Element(name = "temperature")
-    private Temperature temperature;
+  @Element(name = "switch", required = false)
+  private SwitchState switchState;
+  @Element(name = "powermeter", required = false)
+  private PowerMeter powerMeter;
+  @Element(name = "temperature", required = false)
+  private Temperature temperature;
 
-    public String getIdentifier() {
-        return identifier;
-    }
+  public String getIdentifier() {
+    return identifier;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public int getFunctionBitmask() {
-        return functionBitmask;
-    }
+  public int getFunctionBitmask() {
+    return functionBitmask;
+  }
 
-    public String getFirmwareVersion() {
-        return firmwareVersion;
-    }
+  public String getFirmwareVersion() {
+    return firmwareVersion;
+  }
 
-    public String getManufacturer() {
-        return manufacturer;
-    }
+  public String getManufacturer() {
+    return manufacturer;
+  }
 
-    public String getProductName() {
-        return productName;
-    }
+  public String getProductName() {
+    return productName;
+  }
 
-    public boolean isPresent() {
-        return "1".equals(present);
-    }
+  public boolean isPresent() {
+    return "1".equals(present);
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public SwitchState getSwitchState() {
-        return switchState.isNull() ? null : switchState;
-    }
+  public SwitchState getSwitchState() {
+    return switchState.isNull() ? null : switchState;
+  }
 
-    public PowerMeter getPowerMeter() {
-        return powerMeter;
-    }
+  public PowerMeter getPowerMeter() {
+    return powerMeter;
+  }
 
-    public Temperature getTemperature() {
-        return temperature;
-    }
+  public Temperature getTemperature() {
+    return temperature;
+  }
 
-    @Override
-    public String toString() {
-        return "Device [identifier=" + identifier + ", id=" + id + ", functionBitmask=" + functionBitmask
-                + ", firmwareVersion=" + firmwareVersion + ", manufacturer=" + manufacturer + ", productName="
-                + productName + ", present=" + present + ", name=" + name + ", switchState=" + switchState
-                + ", powerMeter=" + powerMeter + ", temperature=" + temperature + "]";
-    }
+  @Override
+  public String toString() {
+    return "Device [identifier=" + identifier + ", id=" + id
+        + ", functionBitmask=" + functionBitmask + ", firmwareVersion="
+        + firmwareVersion + ", manufacturer=" + manufacturer + ", productName="
+        + productName + ", present=" + present + ", name=" + name
+        + ", switchState=" + switchState + ", powerMeter=" + powerMeter
+        + ", temperature=" + temperature + "]";
+  }
 }
