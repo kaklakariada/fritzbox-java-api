0e18daf9ec697c622a33adac925929d293a59cfd
Michael Plate <michael@late.at>
2019-12-20 19:46:21 +0100

    Junit test for connecting and Homeautomatation

---
+++ src/test/java/com/github/kaklakariada/fritzbox/FritzBoxTest.java
@@ -0,0 +1,36 @@
+package com.github.kaklakariada.fritzbox;
+
+import com.github.kaklakariada.fritzbox.model.homeautomation.DeviceList;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class FritzBoxTest {
+
+    private static final String PASSWORD = "";
+    public static final String HTTP_FRITZ_BOX = "http://fritz.box";
+    private static FritzBoxSession fritzBoxSession;
+
+    @Before
+    public void setUp() {
+        fritzBoxSession = new FritzBoxSession(HTTP_FRITZ_BOX);
+        fritzBoxSession.login("", PASSWORD);
+    }
+
+    @Test
+    public void getSessionIdTest() {
+        String sid = fritzBoxSession.getSid();
+        Assert.assertNotNull(sid);
+    }
+
+    @Test
+    public void readAllDevicesTest() {
+        HomeAutomation homeAutomation = HomeAutomation.connect(HTTP_FRITZ_BOX, "", PASSWORD);
+        DeviceList deviceListInfos = homeAutomation.getDeviceListInfos();
+        deviceListInfos.getDevices().stream().forEach(t -> {
+            System.out.println("Identifier: " + t.getIdentifier() + " Name: " + t.getName() + " Temperature: " + t.getTemperature().getCelsius());
+        });
+
+    }
+}
