cecfe30f5055310993f3a23a12b323d14b62462a
janklug <klug.jan@gmail.com>
2021-02-26 00:03:25 +0100

    beautifying

--- src/test/java/com/github/kaklakariada/fritzbox/mapping/DeserializerTest.java
+++ src/test/java/com/github/kaklakariada/fritzbox/mapping/DeserializerTest.java
@@ -22,6 +22,7 @@ import static java.util.stream.Collectors.joining;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 import org.junit.Test;
 
 import com.github.kaklakariada.fritzbox.model.SessionInfo;
